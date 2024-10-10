add5626ae5cef62f9964c6c4cad130c763d4edf1
paledust <paledust@hotmail.com>
2024-10-10 16:50:43 +0800

    rename plane control and add 2D side scroll plane control

--- Assets/Scripts/Player/PlaneControl_Free.cs
+++ Assets/Scripts/Player/PlaneControl_Free.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
-public class PlaneControl : MonoBehaviour
+public class PlaneControl_Free : MonoBehaviour
 {
     [SerializeField] private float maxPitchSpeed;
     [SerializeField] private float maxRollSpeed;
---
+++ Assets/Scripts/Player/PlaneControl_Platform.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl_Platform : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
