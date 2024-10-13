8e3f73050b3efcb37eb16f883ce98777fc660f49
paledust <paledust@hotmail.com>
2024-10-13 23:55:00 +0800

    put in arrow

--- Assets/Scripts/MiniGame/ConductingGameControl.cs
+++ Assets/Scripts/MiniGame/ConductingGameControl.cs
@@ -5,15 +5,17 @@ using UnityEngine.Playables;
 
 public class ConductingGameControl : MonoBehaviour
 {
+[Header("Day Night")]
     [SerializeField] private PlayableDirector timeOfDayDirector;
+[Header("Ref Position")]
     [SerializeField] private Transform entrencePos;
     [SerializeField] private Transform stopPos;
     [SerializeField] private Transform leftExit;
     [SerializeField] private Transform rightExit;
-    void Start()
-    {
-        
-    }
+[Header("Tempo")]
+    [SerializeField] private float beatRate;
+    [SerializeField] private int detectionWindow = 4;
+    [SerializeField] private 
 
     void Update()
     {
