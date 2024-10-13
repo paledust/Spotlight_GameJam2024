26aec89b9afc4958aa4d283d9c450f0c7dcb3695
paledust <paledust@hotmail.com>
2024-10-13 23:34:03 +0800

    add more assets for the control tower

---
+++ Assets/Scripts/MiniGame/ConductingGameControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ConductingGameControl : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector timeOfDayDirector;
+    [SerializeField] private Transform entrencePos;
+    [SerializeField] private Transform stopPos;
+    [SerializeField] private Transform leftExit;
+    [SerializeField] private Transform rightExit;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
