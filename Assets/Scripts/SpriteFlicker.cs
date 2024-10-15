51e6f775f1c410c7e6da62af148b7adccdd01311
paledust <paledust@hotmail.com>
2024-10-16 01:03:24 +0800

    update story for working richard

--- Assets/Scripts/SpriteFlicker.cs
+++ Assets/Scripts/SpriteFlicker.cs
@@ -7,11 +7,13 @@ public class SpriteFlicker : MonoBehaviour
     [SerializeField] private SpriteRenderer m_targetSprite;
     [SerializeField] private Color flickerColor;
     [SerializeField] private float flickerFreq;
+    [SerializeField] private float externalSeed = 0;
+    [SerializeField] private bool autoSeed = true;
     private float seed;
     private Color initColor;
     void Start()
     {
-        seed = Random.Range(0f, 1f);
+        seed = (autoSeed?Random.Range(0f, 1f):0)+externalSeed;
         initColor = m_targetSprite.color;        
     }
     void Update()
