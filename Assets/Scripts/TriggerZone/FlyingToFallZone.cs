72822706b3dde815356fd0dc44d6c562662e5416
paledust <paledust@hotmail.com>
2024-10-23 18:14:32 +0800

    update flying to fall Zone

---
+++ Assets/Scripts/TriggerZone/FlyingToFallZone.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingToFallZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other){
+        
+    }
+}
