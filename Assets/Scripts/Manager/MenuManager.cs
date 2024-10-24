63c1b2aec67ad7cd4f37f3680ada67ad6be6a4fb
paledust <paledust@hotmail.com>
2024-10-24 19:03:32 +0800

    add cursor image

--- Assets/Scripts/Manager/MenuManager.cs
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private Texture2D cursorTex;
+    void Start(){
+        Cursor.visible = true;
+        Cursor.SetCursor(cursorTex, new Vector2(16, 16), CursorMode.Auto);
+    }
     public void ButtonEvent_StartGame(){
+        Cursor.visible = false;
         GameManager.Instance.SwitchingScene("WorkingRichard");
     }
     public void ButtonEvent_EndGame(){
+        Cursor.visible = false;
         GameManager.Instance.EndGame();
     }
 }
