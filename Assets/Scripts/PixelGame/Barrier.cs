842c0260bb6475110ee2d01a6114f77e000fe9fb
NewComer0408 <luxd0408@163.com>
2024-10-27 23:29:19 +0800

    pixel game star count

--- Assets/Scripts/PixelGame/Barrier.cs
+++ Assets/Scripts/PixelGame/Barrier.cs
@@ -18,7 +18,7 @@ public class Barrier : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == Service.PLAYER_TAG)
         {
             PixelGameManager.Instance.ReduceHp();
         }
