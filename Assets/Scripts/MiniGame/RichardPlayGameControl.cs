e18cbe762f593655882b8744e0edb8d3d290ce66
paledust <paledust@hotmail.com>
2024-10-28 17:28:40 +0800

    update richard gaming part sound

--- Assets/Scripts/MiniGame/RichardPlayGameControl.cs
+++ Assets/Scripts/MiniGame/RichardPlayGameControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using SimpleAudioSystem;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
@@ -14,27 +15,49 @@ public class RichardPlayGameControl : MonoBehaviour
     [SerializeField] private float maxPitch;
 [Header("Ending")]
     [SerializeField] private PlayableDirector endTimeline;
+[Header("Audio")]
+    [SerializeField] private AudioSource m_audio;
     
     private float targetPitch;
     private float targetRoll;
     private float pitch = 0;
     private float roll = 0;
+    private bool isGaming = false;
+
     void OnEnable(){
         EventHandler.E_OnPixelGameFinished += EndGame;
     }
     void OnDisable(){
         EventHandler.E_OnPixelGameFinished -= EndGame;
     }
+    void Start(){
+        AudioManager.Instance.PlaySoundEffectLoop(m_audio, "sfx_gaming", 0, 0.1f);
+    }
     void Update(){
     //控制理查的身体晃动
         pitch = Service.LerpValue(pitch, targetPitch, Time.deltaTime*5);
         roll = Service.LerpValue(roll, targetRoll, Time.deltaTime*5);
         richardBody.localRotation = Quaternion.AngleAxis(roll, richardFaceDir.forward)*Quaternion.AngleAxis(pitch, richardFaceDir.right);
+    //控制声效播放
+        if(targetPitch!=0||targetRoll!=0){
+            if(!isGaming){
+                isGaming = true;
+                AudioManager.Instance.FadeAudio(m_audio, 1, 0.1f);
+            }
+        }
+        else{
+            if(isGaming){
+                isGaming = false;
+                AudioManager.Instance.FadeAudio(m_audio, 0, 0.1f, false);
+            }
+        }
     }
     void EndGame(){
         StartCoroutine(coroutineReturnPos(1.5f, 1f));
         playerInput.DeactivateInput();
         endTimeline.Play();
+
+        AudioManager.Instance.FadeAudio(m_audio, 0, 0.1f, true);
     }
     void OnMove(InputValue inputValue){
         Vector2 value = inputValue.Get<Vector2>();
