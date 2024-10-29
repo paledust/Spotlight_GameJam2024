d98796c59b3966daf74ca41a612a6fb107227939
paledust <paledust@hotmail.com>
2024-10-29 16:19:41 +0800

    readding music and falling sound

--- Assets/Scripts/Sound/MusicOnStart.cs
+++ Assets/Scripts/Sound/MusicOnStart.cs
@@ -7,8 +7,8 @@ public class MusicOnStart : MonoBehaviour
 {
     [SerializeField] private string musicName;
     [SerializeField] private float delay = 0;
-    [SerializeField, Range(0, 1)] private float volume;
-    [SerializeField] private float transitionTime;
+    [SerializeField, Range(0, 1)] private float volume = 1;
+    [SerializeField] private float transitionTime = 0.5f;
     void Start()
     {
         StartCoroutine(CommonCoroutine.delayAction(()=> AudioManager.Instance.PlayMusic(musicName, true, transitionTime, volume), delay));
